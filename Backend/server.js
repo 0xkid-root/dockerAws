9b116fe568320abbc06a607aef251c9170e86bfd
0xkid-root <prohackers698@gmail.com>
2026-03-29 22:29:14 +0530

    socket setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,35 @@
-import express from 'express';
-import {createServer} from 'http';
-import {Server} from 'socket.io';
-import {YSocketIO} from 'y-socket.io/dist/server';
-const PORT =8080;
+import express from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
+import { YSocketIO } from "y-socket.io/dist/server"
 
 
-const app = express();
+const app = express()
+app.use(express.static("public"))
 
-const httpServer = createServer(app);
-const io = new Server(httpServer);
 
-app.get("/",(req,res)=>{
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
+    }
+})
+
+
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize()
+
+
+app.get('/health', (req, res) => {
     res.status(200).json({
-        message:"Hello World!",
-        success:true,
+        message: "ok",
+        success: true
     })
 })
 
-app.get('/health',(req,res)=>{
-    res.status(200).json({
-        messsage:"OK!",
-        succcess:true
-    })
-})
 
-httpServer.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
